7403b902f1daad419e11e4efb869389569773b91
Emanuele-01 <emanuele.pitoni.2001@gmail.com>
2023-08-10 16:42:02 +0200

    update

--- d4_w2/src/Main/Main.java
+++ d4_w2/src/Main/Main.java
@@ -11,29 +11,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// prodotti
+		// prodotti  --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		
-		ArrayList<product> prodotti = new ArrayList<product>();
+		ArrayList<Product> prodotti = new ArrayList<Product>();
 		
-		product prodotto1 = new product(123456, "MacBook Pro", "Elettronica",1299.00);
+		Product prodotto1 = new Product(123456, "MacBook Pro", "Elettronica",1299.00);
 		
-		product prodotto2 = new product(789012, "Nike Air Max", "Abbigliamento", 150.00);
+		Product prodotto2 = new Product(789012, "Nike Air Max", "Abbigliamento", 150.00);
 		
-		product prodotto3 = new product(345678, "Samsung Galaxy S21", "Elettronica", 799.00);
+		Product prodotto3 = new Product(345678, "Samsung Galaxy S21", "Elettronica", 799.00);
 		
-		product prodotto4 = new product(901234, "Sony WH-1000XM4", "Elettronica", 349.00);
+		Product prodotto4 = new Product(901234, "Sony WH-1000XM4", "Elettronica", 349.00);
 		
-		product prodotto5 = new product(567890, "Canon EOS R5", "Fotografia", 3899.00);
+		Product prodotto5 = new Product(567890, "Canon EOS R5", "Fotografia", 3899.00);
 		
-		product prodotto6 = new product(234567, "Adidas Ultraboost", "Abbigliamento", 180.00);
+		Product prodotto6 = new Product(234567, "Adidas Ultraboost", "Abbigliamento", 180.00);
 		
-		product prodotto7 = new product(890123, "Apple Watch Series 6", "Elettronica", 399.00);
+		Product prodotto7 = new Product(890123, "Apple Watch Series 6", "Elettronica", 399.00);
 		
-		product prodotto8 = new product(456789, "Google Pixel 5", "Elettronica", 699.00);
+		Product prodotto8 = new Product(456789, "Google Pixel 5", "Elettronica", 699.00);
 		
-		product prodotto9 = new product(246810 , "Sapiens: A Brief History of Humankind", "Books", 144.99);
+		Product prodotto9 = new Product(246810 , "Sapiens: A Brief History of Humankind", "Books", 144.99);
 		
-		product prodotto10 = new product(135791 , "Becoming", "Books", 188.99);
+		Product prodotto10 = new Product(135791 , "Becoming", "Books", 188.99);
 		
 		prodotti.add(prodotto1);
 		prodotti.add(prodotto2);
@@ -46,32 +46,32 @@ public class Main {
 		prodotti.add(prodotto9);
 		prodotti.add(prodotto10);
 		
-		List<String> ricercaBooks = prodotti.stream().filter(book -> book.getNameCategory().equals("Books")).filter(price -> price.getPrice() > 100.00).map(product::getNameProduct).toList();
+		List<String> ricercaBooks = prodotti.stream().filter(book -> book.getNameCategory().equals("Books")).filter(price -> price.getPrice() > 100.00).map(Product::getNameProduct).toList();
 		System.out.println("i libri che costano più di 100 euro sono: " + ricercaBooks);
 		
 		
 		
-		// persone
+		// persone ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		
-		custumer person = new custumer(987654, "Alice", 3245);
+		Customer person = new Customer(987654, "Alice", 3245);
 		
-		custumer person2 = new custumer(234567, "Bob", 4687);
+		Customer person2 = new Customer(234567, "Bob", 4687);
 		
-		custumer person3 = new custumer(876543, "Charlie", 7098);
+		Customer person3 = new Customer(876543, "Charlie", 7098);
 		
-		custumer person4 = new custumer(345678, "Dave", 1567);
+		Customer person4 = new Customer(345678, "Dave", 1567);
 		
-		custumer person5 = new custumer(765432, "Emily", 8312);
+		Customer person5 = new Customer(765432, "Emily", 8312);
 		
 		
 		
-		//ordini
+		//ordini  ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		
-				order ordine1 = new order(543210, "ok", "22.01.2010", "29.01.2010", prodotto1, person);
-				order ordine2 = new order(987654, "error", "08.09.2012", "--", prodotto1, person);
-				order ordine3 = new order(246810, "ok", "14.04.2016", "21.04.2016", prodotto1, person);
-				order ordine4 = new order(135790, "ok", "03.11.2019", "10.11.2019", prodotto1, person);
-				order ordine5 = new order(666999, "error", "28.02.2022", "--", prodotto1, person);
+				Order ordine1 = new Order(543210, "ok", "22.01.2010", "29.01.2010", prodotto1, person);
+				Order ordine2 = new Order(987654, "error", "08.09.2012", "--", prodotto1, person);
+				Order ordine3 = new Order(246810, "ok", "14.04.2016", "21.04.2016", prodotto1, person);
+				Order ordine4 = new Order(135790, "ok", "03.11.2019", "10.11.2019", prodotto1, person);
+				Order ordine5 = new Order(666999, "error", "28.02.2022", "--", prodotto1, person);
 		
 	}
 
